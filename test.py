3b2938cbac6be8d8acc6131a756a62f43e4b502a
Caio Massucato <caiobernardon@gmail.com>
2021-09-14 15:47:43 -0300

    Image encryption and Decryption

--- test.py
+++ test.py
@@ -1,46 +1,40 @@
 # -*- coding: utf-8 -*-
+from numpy import byte
 from aes import AES
 import os
 import io
-import base64
 import binascii
-from PIL import ImageFile, Image
-ImageFile.LOAD_TRUNCATED_IMAGES = True
+from PIL import Image
 from utils import marker_mapping
-from struct import unpack
 
-path = 'test'
-extension = '.jpeg'
-savepath = 'C:=/Users/Caio Massucato/Downloads/'
-
-# Instructions for my AES implication
+# Instructions for AES
 aes = AES(mode='ecb', input_type='data')
         
 # Test vector 128-bit key
 key = '000102030405060708090a0b0c0d0e0f'
 
-# Reads image into list of bytes
-img_file = open("test.jpeg", 'r+b')
-img = img_file.readlines()
-img_file.close()
+# Opens image and reads header and data separately
+img_file = open("test.ppm", "r+b")
+header = img_file.read()[:15]
+img_file = open("test.ppm", "r+b")
+img = img_file.read()[16:]
 
+# Cyphers image data
+cyphertext = aes.encryption(img, key)
 
-# Removes header from byte list and converts
-# it into a byte stream
-bytestream = b''.join(img)
-byte_data = bytestream.split(sep=b'\xff\xda')
-img_data = byte_data[1].strip(b'\xff\xd9')
+# Concat header and cyphered image data
+cypherstream = header +  cyphertext
 
-# # Encrypts the byte stream
-cyphertext = aes.encryption(img_data, key)
-cypherstream = byte_data[0] + b'\xff\xda' + cyphertext + b'\xff\xd9'
+# Create output file containing scrambled image bytes
+cypher_file = open("out.ppm", "w+b")
+cypher_file.write(cypherstream)
 
-cypher_img = Image.open(io.BytesIO(cypherstream))
-cypher_img.load()
-cypher_img.show()
-cypher_img.close()
+# Decyphers image data
+decyphertext = aes.decryption(cyphertext, key)
+decypherstream = header + decyphertext
+print(decypherstream)
 
-# # Decrypts the byte stream
-# plaintext = aes.decryption(bytestream, key)
-
+# Create output file containing unscrambled image bytes
+decypher_file = open("out2.ppm", "w+b")
+decypher_file.write(decypherstream)
 
